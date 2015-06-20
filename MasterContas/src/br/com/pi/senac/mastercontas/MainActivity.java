a4e3432d1928746dfd8003f7d5eaf4328fbb4d42
Herdeson <herdeson.1@gmail.com>
2015-06-20 11:41:21 -0300

    Primeiro Commit

---
+++ MasterContas/src/br/com/pi/senac/mastercontas/MainActivity.java
@@ -0,0 +1,29 @@
+package br.com.pi.senac.mastercontas;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void selecionaOpcao(View view){
+		switch (view.getId()) {
+		case R.id.cadDevedor:
+			startActivity(new Intent(this, DevedorActivity.class));
+			break;
+
+		default:
+			break;
+		}
+	}
+
+}
